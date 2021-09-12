89c4132a9f30900efbfc66bb7ea2ab16d1486b4c
fuzziejaxer <69056690+fuzziejaxer@users.noreply.github.com>
2021-09-12 22:23:27 +0100

    Create maths.js

---
+++ maths.js
@@ -0,0 +1,39 @@
+// Numbers In JS
+
+/*
+Adding Numbers
+*/
+
+var add = 10 + 10;
+
+/*
+Taking Away Numbers
+*/
+
+var subtract = 10 - 5;
+
+/*
+Timseing Numbers
+*/
+
+var times = 5 * 5;
+
+/*
+Dividing Numbers
+*/
+
+var divide = 80 / 10;
+
+/*
+Add 1 Onto A Variable
+*/
+
+var num = 10;
+num++;
+
+/*
+Taking Away 1 From A Variable
+*/
+
+var int = 11;
+int--;
