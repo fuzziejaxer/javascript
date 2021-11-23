c17a022873e794c0ec56ed3270281f5ee1addc89
FuzzieJaxer <69056690+fuzziejaxer@users.noreply.github.com>
2021-11-23 16:42:50 +0000

    Create if.js

---
+++ if.js
@@ -0,0 +1,16 @@
+//<html>
+//<head>
+//<title>JS</title>
+//</head>
+//<body>
+//<script>
+let food = window.prompt("Enter Your Favourite Food: ");
+
+if (food = "pizza") {
+    document.write("Good Choice!");
+} else {
+    document.write("Disgusting");
+}
+//</script>
+//</body>
+//</html>
