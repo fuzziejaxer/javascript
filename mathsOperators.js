615507bb9db79fe7c258d24675d39b6665afaceb
FuzzieJaxer <69056690+fuzziejaxer@users.noreply.github.com>
2022-05-22 11:21:29 +0100

    Create mathsOperators.js

---
+++ mathsOperators.js
@@ -0,0 +1,28 @@
+i = 0;
+hello = true;
+hi = "hi";
+
+// greater or equals
+if (i >= 0) {
+    console.log('i is greater than or equal to 0');
+}
+
+// less or equals
+if (i <= 0) {
+
+}
+
+// not equals
+if (i != 0) {
+
+}
+
+// equals
+if (hello == true) {
+
+}
+
+// equals string
+if (hi == "hi") {
+
+}
